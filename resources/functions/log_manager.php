f2877863542875cb471a87b1faeb4a93a559663d
mhartopo <udaraapi@rocketmail.com>
2016-04-15 02:32:39 +0700

    log manager

---
+++ resources/functions/log_manager.php
@@ -0,0 +1,19 @@
+<?php
+
+	function writeLog($date, $type, $message) {
+		$log_file = realpath(dirname(__FILE__)."/../log.txt");
+		$current = file_get_contents($log_file);
+		$content = $current.$date." - ".$type." - ".$message."\n";
+		file_put_contents($log_file, $content, FILE_APPEND | LOCK_EX);
+	}
+	function eraseLog() {
+		$log_file = realpath(dirname(__FILE__)."/../log.txt");
+		$content = "";
+		file_put_contents($log_file, $content, FILE_APPEND | LOCK_EX);
+	}
+	
+	function getLogContent() {
+		$log_file = realpath(dirname(__FILE__)."/../log.txt");
+		return file_get_contents($log_file);
+	}
+?>
