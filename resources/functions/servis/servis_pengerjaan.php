566a59546d9bcc71780606fe256e95d450812686
mhartopo <udaraapi@rocketmail.com>
2016-04-14 23:28:25 +0700

    servis_pengerjaan servis_selesai CRUD

--- resources/functions/servis/servis_pengerjaan.php
+++ resources/functions/servis/servis_pengerjaan.php
@@ -1,31 +1,117 @@
 <?php 
+
+	/*
+		Modul servis pengerjaan
+
+	*/
 	require_once(realpath(dirname(__FILE__) . "../../../config.php"));
 
-	function addServisPengerjaan($idLaundry, $jenis, $jumlah) {
 
+	function addServisPengerjaan($id_laundry, $jenis, $jumlah_cucian) {
+		global $db;
+		try {
+			$stmt = $db->prepare("SELECT * FROM servis_pengerjaan WHERE jenis=:jenis and id_laundry = :id_laundry");
+			$stmt->bindParam(':jenis', $jenis);
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->execute();
+
+			if($stmt->rowCount() == 0) {
+				$stmt = $db->prepare("INSERT INTO servis_pengerjaan (id_laundry, jenis, harga) VALUES(:id_laundry, :jenis, :harga)");
+				$stmt->bindParam(':id_laundry', $id_laundry);
+				$stmt->bindParam(':jenis',$jenis);
+				$stmt->bindParam(':harga',$harga);
+				$stmt->execute();
+				return true;
+			} else {
+				return false;
+			}
+
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
 	function getServisPengerjaan() {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_pengerjaan");
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function getServisPengerjaan($idLaundry) {
+	function getServisPengerjaan($id_laundry) {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_pengerjaan WHERE id_laundry = :id_laundry");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
 	function getServisPengerjaan($jenis) {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_pengerjaan WHERE jenis = :jenis");
+			$stmt->bindParam(':jenis', $jenis);
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function getServisPengerjaan($idLaundry, $jenis) {
+	function getServisPengerjaan($id_laundry, $jenis) {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_pengerjaan WHERE id_laundry = :id_laundry AND jenis = :jenis");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->bindParam(':jenis', $jenis);
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function deleteServisPengerjaan($idLaundry, $jenis) {
-
+	function deleteServisPengerjaan($id_laundry, $jenis) {
+		global $db;
+		try {
+			$stmt = $db->prepare("DELETE servis_pengerjaan WHERE id_laundry = :id_laundry AND jenis = :jenis");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->bindParam(':jenis', $jenis);
+			$stmt->execute();
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function updateServicePengerjaan($idLaundry, $jenis) {
-		
+	function updateServicePengerjaan($id_laundry, $jenis, $jumlah_cucian) {
+		global $db;
+		try {
+			$stmt = $db->prepare("UPDATE servis_pengerjaan SET jumlah_cucian = :jumlah_cucian WHERE id_laundry = :id_laundry AND jenis = :jenis");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->bindParam(':jenis', $jenis);
+			$stmt->bindParam(':jumlah_cucian', $jumlah_cucian);
+			$stmt->execute();
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 ?>
--- resources/functions/servis/servis_selesai.php
+++ resources/functions/servis/servis_selesai.php
@@ -1,30 +1,117 @@
-<?php
-	require_once(realpath(dirname(__FILE__) . "../../../config.php"));
-	function addServisSelesai($idLaundry, $jenis, $jumlah) {
+<?php 
 
+	/*
+		Modul servis Selesai
+
+	*/
+	require_once(realpath(dirname(__FILE__) . "../../../config.php"));
+
+
+	function addServisSelesai($id_laundry, $jenis, $jumlah_cucian) {
+		global $db;
+		try {
+			$stmt = $db->prepare("SELECT * FROM servis_selesai WHERE jenis=:jenis and id_laundry = :id_laundry");
+			$stmt->bindParam(':jenis', $jenis);
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->execute();
+
+			if($stmt->rowCount() == 0) {
+				$stmt = $db->prepare("INSERT INTO servis_selesai (id_laundry, jenis, harga) VALUES(:id_laundry, :jenis, :harga)");
+				$stmt->bindParam(':id_laundry', $id_laundry);
+				$stmt->bindParam(':jenis',$jenis);
+				$stmt->bindParam(':harga',$harga);
+				$stmt->execute();
+				return true;
+			} else {
+				return false;
+			}
+
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
 	function getServisSelesai() {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_selesai");
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function getServisSelesai($idLaundry) {
+	function getServisSelesai($id_laundry) {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_selesai WHERE id_laundry = :id_laundry");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
 	function getServisSelesai($jenis) {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_selesai WHERE jenis = :jenis");
+			$stmt->bindParam(':jenis', $jenis);
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function getServisSelesai($idLaundry, $jenis) {
+	function getServisSelesai($id_laundry, $jenis) {
+		global $db;
+		try {
+			
+			$stmt = $db->prepare("SELECT * FROM servis_selesai WHERE id_laundry = :id_laundry AND jenis = :jenis");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->bindParam(':jenis', $jenis);
+			return $stmt->fetchAll();
 
+		}catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function deleteServisSelesai($idLaundry, $jenis) {
-
+	function deleteServisSelesai($id_laundry, $jenis) {
+		global $db;
+		try {
+			$stmt = $db->prepare("DELETE servis_selesai WHERE id_laundry = :id_laundry AND jenis = :jenis");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->bindParam(':jenis', $jenis);
+			$stmt->execute();
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 
-	function updateServiceSelesai($idLaundry, $jenis) {
-		
+	function updateServisSelesai($id_laundry, $jenis, $jumlah_cucian) {
+		global $db;
+		try {
+			$stmt = $db->prepare("UPDATE servis_selesai SET jumlah_cucian = :jumlah_cucian WHERE id_laundry = :id_laundry AND jenis = :jenis");
+			$stmt->bindParam(':id_laundry', $id_laundry);
+			$stmt->bindParam(':jenis', $jenis);
+			$stmt->bindParam(':jumlah_cucian', $jumlah_cucian);
+			$stmt->execute();
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$db = null;
 	}
 ?>
